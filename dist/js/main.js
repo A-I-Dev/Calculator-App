24ce2ab694acaec834725ce447589e4e0c1efdfc
A-I-Dev <aleksandar_ivanov@yahoo.com>
2020-06-08 20:03:32 +0300

    Finished the Two Numbers Calculator v1.0

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,7 +3,7 @@
 const calcKeys = document.getElementsByClassName("calc-key"); 
 const resultField = document.getElementById("result"); 
 const allDigits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-const allOperators = ["+", "*", "/", "-"];
+const allOperators = ["/", "*", "+", "-"];
 
 for (calcKey of calcKeys) {
     calcKey.addEventListener("click", calcKeysClick);
@@ -39,11 +39,12 @@ function calcKeysClick() {
 }
 
 function digitKeysClick(keyElement) {
-    let resultFieldText = resultField.textContent;
-
-    let lastCharacter = resultFieldText.charAt(resultFieldText.length - 1);
-    let characterBeforeLast = resultFieldText.charAt(resultFieldText.length - 2);    
-    let thisKeyElementText = keyElement.firstElementChild.textContent;
+    let 
+        resultFieldText = resultField.textContent,
+        lastCharacter = resultFieldText.charAt(resultFieldText.length - 1),
+        characterBeforeLast = resultFieldText.charAt(resultFieldText.length - 2),
+        thisKeyElementText = keyElement.firstElementChild.textContent
+    ;
 
     if (keyElement.id === "zero-key") {
         if (
@@ -75,26 +76,51 @@ function digitKeysClick(keyElement) {
 }
 
 function operatorKeysClick(keyElement) {
-    let resultFieldText = resultField.textContent;
-    let lastCharacter = resultFieldText.charAt(resultFieldText.length - 1);
-    let characterBeforeLast = resultFieldText.charAt(resultFieldText.length - 2);
-    let thisKeyElementText = keyElement.firstElementChild.textContent;
+    let 
+        resultFieldText = resultField.textContent,
+        lastCharacter = resultFieldText.charAt(resultFieldText.length - 1),
+        characterBeforeLast = resultFieldText.charAt(resultFieldText.length - 2),
+        thisKeyElementText = keyElement.firstElementChild.textContent,
+        firstCharacterIsMinus = (resultFieldText.charAt(0) === "-"),
+        thereIsAnOperator = false
+    ;
 
-    if (
-        allOperators.includes(lastCharacter) 
-        || (lastCharacter === "." && allOperators.includes(characterBeforeLast)) 
-        || (resultFieldText === "" && thisKeyElementText !== "-")
-    ) {
+    for (let i = 0; i < resultFieldText.length; i++) {
+        if (firstCharacterIsMinus) {
+            firstCharacterIsMinus = false;
+            continue;
+        }
+        else {
+            if (allOperators.includes(resultFieldText[i])) {
+                thereIsAnOperator = true;
+                break;
+            }
+            else {
+                continue;
+            }
+        } 
+    }
+
+    if (thereIsAnOperator) {
         resultFieldText = resultFieldText;
     }
-    else if (
-        lastCharacter === "." 
-        && !allOperators.includes(characterBeforeLast)
-    ) {
-        resultFieldText = resultFieldText.slice(0, resultFieldText.length - 1) + thisKeyElementText;
-    }
     else {
-        resultFieldText += thisKeyElementText;
+        if (
+            allOperators.includes(lastCharacter) 
+            || (lastCharacter === "." && allOperators.includes(characterBeforeLast)) 
+            || (resultFieldText === "" && thisKeyElementText !== "-")
+        ) {
+            resultFieldText = resultFieldText;
+        }
+        else if (
+            lastCharacter === "." 
+            && !allOperators.includes(characterBeforeLast)
+        ) {
+            resultFieldText = resultFieldText.slice(0, resultFieldText.length - 1) + thisKeyElementText;
+        }
+        else {
+            resultFieldText += thisKeyElementText;
+        }
     }
 
     return resultFieldText;
@@ -142,37 +168,42 @@ function clearKeysClick(keyElement) {
     let resultFieldText = resultField.textContent;
 
     if (keyElement.id === "clear-key") {
-        resultFieldText = "";
+        resultFieldText = 0;
     }
     else {
         resultFieldText = resultFieldText.slice(0, resultFieldText.length - 1);
+        if (resultFieldText === "") {
+            resultFieldText = 0;
+        }
     }
 
     return resultFieldText;
 }
 
 function printResult() {
-    let resultFieldText = resultField.textContent;
-    let lastCharacter = resultFieldText.charAt(resultFieldText.length - 1);
-    let characterBeforeLast = resultFieldText.charAt(resultFieldText.length - 2);  
+    let 
+        resultFieldText = resultField.textContent,
+        lastCharacter = resultFieldText.charAt(resultFieldText.length - 1),
+        characterBeforeLast = resultFieldText.charAt(resultFieldText.length - 2)
+    ;
 
     if (
         !allOperators.includes(lastCharacter) 
         && lastCharacter !== "."
     ) {
-        resultFieldText = eval(resultFieldText);
+        resultFieldText = calculate(resultFieldText);
     }
     else if (
         allOperators.includes(lastCharacter) 
         || (lastCharacter === "." && allDigits.includes(characterBeforeLast))
     ) {
-        resultFieldText = eval(resultFieldText.slice(0, resultFieldText.length - 1));
+        resultFieldText = calculate(resultFieldText.slice(0, resultFieldText.length - 1));
     }
     else if (
         lastCharacter === "." 
         && allOperators.includes(characterBeforeLast)
     ) {
-        resultFieldText = eval(resultFieldText.slice(0, resultFieldText.length - 2));
+        resultFieldText = calculate(resultFieldText.slice(0, resultFieldText.length - 2));
     }
     else {
         resultFieldText = resultFieldText;
@@ -183,46 +214,104 @@ function printResult() {
 
 
 
-///////////////////////////
-// CALCULATION FUNCTION //
-/////////////////////////
+//////////////////
+// CALCULATION //
+////////////////
 
-// function calc(content) {
-//     let result = "";
-//     let nums = [];
-//     let mathSigns = [];
-//     let resultWindowContent = content;
-//     let counter = 0;
+function calculate(resultFieldText) {
+    let 
+        nums = numbersCollector(resultFieldText),
+        inputOperators = operatorsCollector(resultFieldText),
+        result = resultFieldText,
+        power = 0,
+        thereIsOneOrMoreFloatingPointNumbers = false
+    ;
 
-//     for (let i  = 0; i < resultWindowContent.length; i++) {
-//         if (resultWindowContent[i] === "+" || resultWindowContent[i] === "-" || resultWindowContent[i] === "*" || resultWindowContent[i] === "/") {
-//             mathSigns[counter] = resultWindowContent[i];
-//             counter++;
-//         }
-//         else {
-//             nums[counter] += resultWindowContent[i];
-//         }
-//     }
+    if (nums[0] !== undefined && nums[1] !== undefined) {
+        for (num of nums) {
+            if (num.indexOf(".") !== -1) {
+                if (power < (num.length - 1) - num.indexOf(".")) {
+                    power = (num.length - 1) - num.indexOf(".");
+                }
+            }
+        }
 
-//     for (let i = 0; i < nums.length; i++) {
-//         nums[i] = Number(nums[i].slice(9, nums[i].length));
-//     }
+        if (nums[0].indexOf(".") !== -1 || nums[1].indexOf(".") !== -1) {
+            thereIsOneOrMoreFloatingPointNumbers = true;
+            nums[0] = Math.floor(parseFloat(nums[0]) * (Math.pow(10, power)));
+            nums[1] = Math.floor(parseFloat(nums[1]) * (Math.pow(10, power)));
+        }
+        else {
+            nums[0] = parseInt(nums[0]);
+            nums[1] = parseInt(nums[1]);
+        }
 
-//     for (let i = 0; i < nums.length; i++) {
-//         if (mathSigns[i] !== undefined) {
-//             result += nums[i] + mathSigns[i];
-//         }
-//         else {
-//             result += nums[i];
-//         }
-//     }
+        switch (inputOperators[0]) {
+            case "/":
+                result = nums[0] / nums[1];
+                break;
+            case "*":
+                result = nums[0] * nums[1];
+                if (thereIsOneOrMoreFloatingPointNumbers) {
+                    result /= Math.pow(10, power * 2);
+                }
+                break;
+            case "+":
+                result = nums[0] + nums[1];
+                if (thereIsOneOrMoreFloatingPointNumbers) {
+                    result /= Math.pow(10, power);
+                }
+                break;
+            case "-":
+                result = nums[0] - nums[1];
+                if (thereIsOneOrMoreFloatingPointNumbers) {
+                    result /= Math.pow(10, power);
+                }
+                break;
+        }
+    }
 
-//     console.log(nums);
-//     console.log(mathSigns);
-//     console.log(result);
-//     console.log(typeof(result));
+    return result;
+}
 
-//     return result;
-// }
+function numbersCollector(resultFieldText) {
+    let collectorsCounter = 0;
+    let nums = ["", ""];
+
+    for (let i  = 0; i < resultFieldText.length; i++) {
+        if (resultFieldText[i] === "-" && i === 0) {
+            nums[collectorsCounter] += resultFieldText[i];
+        }
+        else {
+            if (allOperators.includes(resultFieldText[i])) {
+                collectorsCounter++;
+            }
+            else {
+                nums[collectorsCounter] += resultFieldText[i];
+            }
+        }
+    }
+
+    return nums;
+}
+
+function operatorsCollector(resultFieldText) {
+    let collectorsCounter = 0;
+    let inputOperators = [];
+
+    for (let i  = 0; i < resultFieldText.length; i++) {
+        if (resultFieldText[i] === "-" && i === 0) {
+            continue;
+        }
+        else {
+            if (allOperators.includes(resultFieldText[i])) {
+                inputOperators[collectorsCounter] = resultFieldText[i];
+                collectorsCounter++;
+            }
+        }
+    }
+
+    return inputOperators;
+}
 
 })();
